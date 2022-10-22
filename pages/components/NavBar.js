203dc006b39fc70e51c51e0f3fba44608f286c50
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-22 16:54:43 +0100

    made nav items white

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -6,7 +6,7 @@ import { Navbar } from 'react-bootstrap';
 const NavBar = () => {
   return (
     <>
-      <Navbar bg="dark" expand="lg">
+      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
         <Container>
           <Navbar.Brand href="#home">
             <Image
@@ -15,13 +15,35 @@ const NavBar = () => {
               height="64"
             ></Image>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link  href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+          {/* <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto"> */}
+          <Nav.Link className="nav-link-white" href="#">
+            TV
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Movies
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Store
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Games
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Video
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Win
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Holidays
+          </Nav.Link>
+          <Nav.Link className="nav-link-white" href="#">
+            Disneylife
+          </Nav.Link>
+          {/* </Nav>
+          </Navbar.Collapse> */}
         </Container>
       </Navbar>
     </>
