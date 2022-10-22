6aa7cb8ad1baa12cc8199a8d7490fa31ef55fa45
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-22 17:30:33 +0100

    add fontawesome icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 // custom app wrapper to include boostrap globally
 // see here for more details: https://nextjs.org/docs/basic-features/built-in-css-support
 import 'bootstrap/dist/css/bootstrap.css';
-import '/public/static/css/index.css'
+import '/public/static/css/index.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 const _App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
