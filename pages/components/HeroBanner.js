28b2f3d9c30426870382335782269e5a383dfea0
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-28 22:33:30 +0100

    add filter to carousel images

--- pages/components/HeroBanner.js
+++ pages/components/HeroBanner.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Carousel from 'react-bootstrap/Carousel';
+import BuyNowButton from './BuyNowButton';
 
 const HeroBanner = () => {
   return (
@@ -7,6 +8,7 @@ const HeroBanner = () => {
       <Carousel.Item className="hero-banner-image">
         <Image
           src="/static/images/swfa-wide.jpg"
+          className="hero-banner-image-filter"
           width={700}
           height={475}
           sizes="100vw"
@@ -17,13 +19,17 @@ const HeroBanner = () => {
           }}
         ></Image>
         <Carousel.Caption>
-          <h2>Star Wars: The Force Awakens</h2>
-          <p>Available to Rent or Buy Now</p>
+          <div className="hero-banner-vertical-center">
+            <h2>Star Wars: The Force Awakens</h2>
+            <h4>Available to Rent or Buy Now</h4>
+            <BuyNowButton />
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="hero-banner-image">
         <Image
           src="/static/images/swtlj-wide.jpg"
+          className="hero-banner-image-filter"
           width={700}
           height={475}
           sizes="100vw"
@@ -34,13 +40,17 @@ const HeroBanner = () => {
           }}
         ></Image>
         <Carousel.Caption>
-          <h2>Star Wars: The Last Jedi</h2>
-          <p>Available to Rent or Buy Now</p>
+          <div className="hero-banner-vertical-center">
+            <h2>Star Wars: The Last Jedi</h2>
+            <h4>Available to Rent or Buy Now</h4>
+            <BuyNowButton />
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="hero-banner-image">
         <Image
           src="/static/images/swtros-wide.webp"
+          className="hero-banner-image-filter"
           width={700}
           height={475}
           sizes="100vw"
@@ -51,8 +61,11 @@ const HeroBanner = () => {
           }}
         ></Image>
         <Carousel.Caption>
-          <h2>Star Wars: The Rise of Skywalker</h2>
-          <p>Available to Rent or Buy Now</p>
+          <div className="hero-banner-vertical-center">
+            <h2>Star Wars: The Rise of Skywalker</h2>
+            <h4>Available to Rent or Buy Now</h4>
+            <BuyNowButton />
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
