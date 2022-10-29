5b3b20be1d36bb1fadba6d7938b0ded5269977c8
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-29 10:21:11 +0100

    add new release component

---
+++ pages/components/NewReleases.js
@@ -0,0 +1,14 @@
+const NewReleases = () => {
+  return (
+    <div className="newreleases-container">
+      <div className="newreleases-top-row">
+        <h2>New Releases This Month</h2>
+        <button className="button-rounded button-padding">
+          See all
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewReleases;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import HeroBanner from './components/HeroBanner';
 import NavBar from './components/NavBar';
+import NewReleases from './components/NewReleases';
 import React from 'react';
 
 const Index = () => {
@@ -8,10 +9,20 @@ const Index = () => {
       <React.StrictMode>
         <NavBar />
         <HeroBanner />
-        <div className='text-shoutout text-center'>
+        <div className="text-shoutout text-center">
           <h2>Covid-19 Update</h2>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus laudantium accusantium, corrupti tenetur, deleniti consequatur quae enim ullam quibusdam iste voluptate, nemo iusto animi. Illum nihil et totam ducimus error? Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum inventore molestias rem voluptate nisi repellendus quibusdam, necessitatibus expedita. Est optio similique rem asperiores quidem doloribus recusandae exercitationem vitae dolore minima?</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
+            laudantium accusantium, corrupti tenetur, deleniti consequatur quae
+            enim ullam quibusdam iste voluptate, nemo iusto animi. Illum nihil
+            et totam ducimus error? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Rerum inventore molestias rem voluptate nisi
+            repellendus quibusdam, necessitatibus expedita. Est optio similique
+            rem asperiores quidem doloribus recusandae exercitationem vitae
+            dolore minima?
+          </p>
         </div>
+        <NewReleases />
       </React.StrictMode>
     </div>
   );
