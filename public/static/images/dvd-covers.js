2d78452b61f7bed2b8835f261d1f52b88581d8e0
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-30 11:43:18 +0000

    create dvd-covers export file

---
+++ public/static/images/dvd-covers.js
@@ -0,0 +1,29 @@
+import beauty from './dvd-covers/dvd-beauty.webp';
+import cinderella from './dvd-covers/dvd-cinderella.webp';
+import encanto from './dvd-covers/dvd-encanto.webp';
+import frozen2 from './dvd-covers/dvd-frozen2.webp';
+import hunch from './dvd-covers/dvd-hunch.webp';
+import lilo from './dvd-covers/dvd-lilo.webp';
+import luca from './dvd-covers/dvd-luca.webp';
+import moana from './dvd-covers/dvd-moana.webp';
+import peter from './dvd-covers/dvd-peter.webp';
+import ralph from './dvd-covers/dvd-ralph.webp';
+import turningred from './dvd-covers/dvd-turningred.webp';
+import winnie from './dvd-covers/dvd-winnie.webp';
+
+const images = [
+  beauty,
+  cinderella,
+  encanto,
+  frozen2,
+  hunch,
+  lilo,
+  luca,
+  moana,
+  peter,
+  ralph,
+  turningred,
+  winnie,
+];
+
+export default images;
