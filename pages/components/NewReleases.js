e6af46a260cfea729478be64b6439706dd45edf1
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-30 11:43:44 +0000

    create horizontal 3x1 carousel for dvd-covers

--- pages/components/NewReleases.js
+++ pages/components/NewReleases.js
@@ -1,11 +1,68 @@
+import images from '../../public/static/images/dvd-covers';
+
+import Image from 'next/image';
+import Carousel from 'react-bootstrap/Carousel';
+import Stack from 'react-bootstrap/Stack';
+import Card from 'react-bootstrap/Card';
+
 const NewReleases = () => {
+  const renderImages = images.map((img, index) => (
+    <Card>
+      <Image
+        src={img}
+        key={index}
+        width=""
+        height=""
+        style={{ width: '100%', height: 'auto' }}
+      ></Image>
+    </Card>
+  ));
+
   return (
     <div className="newreleases-container">
       <div className="newreleases-top-row">
         <h2>New Releases This Month</h2>
-        <button className="button-rounded button-padding">
-          See all
-        </button>
+        <button className="button-rounded button-padding">See all</button>
+      </div>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <Stack
+              direction="horizontal"
+              className="justify-content-center align-items-center"
+              gap={3}
+            >
+              {renderImages.slice(0, 3)}
+            </Stack>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Stack
+              direction="horizontal"
+              className="justify-content-center align-items-center"
+              gap={3}
+            >
+              {renderImages.slice(3, 6)}
+            </Stack>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Stack
+              direction="horizontal"
+              className="h-100 justify-content-center align-items-center"
+              gap={3}
+            >
+              {renderImages.slice(6, 9)}
+            </Stack>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Stack
+              direction="horizontal"
+              className="justify-content-center align-items-center"
+              gap={3}
+            >
+              {renderImages.slice(9, 12)}
+            </Stack>
+          </Carousel.Item>
+        </Carousel>
       </div>
     </div>
   );
